8f05d8550223601419c1e3f8e100e099f377c284
kohei.saito <fu.saito.nf.k@gmail.com>
2023-10-12 22:48:52 +0900

    Userコントローラー作成し直し

--- mot2/app/Http/Controllers/UserController.php
+++ mot2/app/Http/Controllers/UserController.php
@@ -6,42 +6,59 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /*
-     * ユーザー一覧
+    /**
+     * Display a listing of the resource.
      */
-    public function list()
+    public function index()
     {
-        echo 'ユーザー一覧です';
-    }
-    /*
-     * ユーザー一覧
-     */
-    public function show()
-    {
-        echo 'ユーザー一覧';
+        //
     }
 
-    /*
-     * ユーザー新規作成
+    /**
+     * Show the form for creating a new resource.
      */
     public function create()
     {
-        echo 'ユーザー新規登録';
+        //
     }
 
-    /*
-     * ユーザー編集
+    /**
+     * Store a newly created resource in storage.
      */
-    public function edit()
+    public function store(Request $request)
     {
-        echo 'ユーザー一覧です';
+        //
     }
 
-    /*
-     * ユーザー削除
+    /**
+     * Display the specified resource.
      */
-    public function delete()
+    public function show(string $id)
     {
-        echo 'ユーザー削除';
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }

18a63d8ab02247d9d8d3d5bab05eb11c9de8e92a
kohei.saito <fu.saito.nf.k@gmail.com>
2023-10-23 23:30:14 +0900

    topicsテーブル作成用のマイグレーションファイルの更新とマイグレーションの実行

--- mot2/database/migrations/2023_10_23_230928_create_topics_table.php
+++ mot2/database/migrations/2023_10_23_230928_create_topics_table.php
@@ -12,8 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('topics', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id')->comment('ID');
+            $table->string('title', 50)->nullable(false)->comment('件名');
+            $table->string('text', 500)->nullable(false)->comment('本文');
+            $table->integer('user_id')->nullable(false)->comment('投稿者');
+            $table->datetime('posted_at')->useCurrent()->comment('作成日時');
+            $table->datetime('updated_at')->useCurrentOnUpdate()->comment('更新日時');
+            $table->boolean('is_deleted')->default(false)->comment('論理削除フラグ true:論理削除');
         });
     }
 

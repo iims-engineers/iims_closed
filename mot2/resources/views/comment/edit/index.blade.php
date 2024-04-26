9395dcf6d084eff126b6ec6de223859de5c6b55b
kohei.saito <fu.saito.nf.k@gmail.com>
2024-04-26 23:03:13 +0900

    コメント機能の調整

--- mot2/resources/views/comment/edit/index.blade.php
+++ mot2/resources/views/comment/edit/index.blade.php
@@ -43,69 +43,72 @@
               @if(data_get($comment, 'id') === data_get($target_comment, 'id'))
               {{-- 編集対象のコメントの場合はコメント欄最下部に配置するためスキップ --}}
               @else
-              <div class="c-user no-link">
-                <div class="c-user-icon">
-                  <!-- <img src="{{ data_get($comment, 'user_icon', '') }}" alt=""> -->
-                  <img src="/img/common/dummy_icon.png" alt="">
+              <div class="c-reply c-reply--has-detail">
+                <div class="c-user no-link">
+                  <div class="c-user-icon">
+                    <!-- <img src="{{ data_get($comment, 'user_icon', '') }}" alt=""> -->
+                    <img src="/img/common/dummy_icon.png" alt="">
+                  </div>
+                  <div class="c-user-info">
+                    <div class="c-user-name">{{ data_get($comment, 'username') }}</div>
+                    <div class="c-user-id">@ {{ data_get($comment, 'user_identifier') }}</div>
+                  </div>
                 </div>
-                <div class="c-user-info">
-                  <div class="c-user-name">{{ data_get($comment, 'username') }}</div>
-                  <div class="c-user-id">@ {{ data_get($comment, 'user_identifier') }}</div>
+                <div class="c-reply-detail">
+                  <p>
+                    {!! nl2br(htmlspecialchars(data_get($comment, 'comment'))) !!}
+                  </p>
+                  <time class="c-reply-date" datetime="{{ data_get($comment, 'created_at') }}">{{ data_get($comment, 'created_at') }}</time>
+                  @if(data_get($comment, 'user_id') === $user_id)
+                  <div class="c-reply-edit">
+                    <a href="{{ route('comment.show.edit', ['comment_id' => data_get($comment, 'id')]) }}" class="c-button--mini">
+                      <img src="/img/common/icon-pencil.svg" alt="">
+                      <span>回答を編集</span>
+                    </a>
+                  </div>
+                  @endif
                 </div>
               </div>
-              <div class="c-reply-detail">
-                <p>
-                  {!! nl2br(htmlspecialchars(data_get($comment, 'comment'))) !!}
-                </p>
-                <time class="c-reply-date" datetime="{{ data_get($comment, 'created_at') }}">{{ data_get($comment, 'created_at') }}</time>
-                @if(data_get($comment, 'user_id') === $user_id)
-                <div class="c-reply-edit">
-                  <a href="{{ route('comment.show.edit', ['comment_id' => data_get($comment, 'id')]) }}" class="c-button--mini">
-                    <img src="/img/common/icon-pencil.svg" alt="">
-                    <span>回答を編集</span>
-                  </a>
-                </div>
-                @endif
-                @endif
-                @endforeach
-                @endif
-                {{-- 編集対象のコメント --}}
-                <div class="c-reply c-reply--has-detail">
-                  <form action="{{ route('comment.store') }}" method="POST" class="c-form" style="width: 100%;">
-                    @csrf
-                    <div class="c-form-item">
-                      <div class="c-user no-link">
-                        <div class="c-user-icon">
-                          <!-- <img src="{{ data_get($target_comment, 'user_icon', '') }}" alt=""> -->
-                          <img src="/img/common/dummy_icon.png" alt="">
-                        </div>
-                        <div class="c-user-info">
-                          <div class="c-user-name">{{ data_get($target_comment, 'username') }}</div>
-                          <div class="c-user-id">@ {{ data_get($target_comment, 'user_identifier') }}</div>
-                        </div>
+              @endif
+              @endforeach
+              @endif
+              {{-- 編集対象のコメント --}}
+              <div class="c-reply c-reply--has-detail">
+                <form action="{{ route('comment.store') }}" method="POST" class="c-form" style="width: 100%;">
+                  @csrf
+                  <div class="c-form-item">
+                    <div class="c-user no-link">
+                      <div class="c-user-icon">
+                        <!-- <img src="{{ data_get($target_comment, 'user_icon', '') }}" alt=""> -->
+                        <img src="/img/common/dummy_icon.png" alt="">
                       </div>
-                      <!-- <textarea name="comment" id="comment" cols="30" rows="10">{!! nl2br(htmlspecialchars(data_get($target_comment, 'comment'))) !!}</textarea> -->
-                      <textarea name="comment" id="comment" cols="30" rows="10">{{ data_get($target_comment, 'comment') }}</textarea>
-                      @error('comment')
-                      <p class="error-text">※{{ $message }}</p>
-                      @enderror
-                      <input type="hidden" name="topic_id" value="{{ data_get($topic, 'id') }}">
-                      <input type="hidden" name="comment_id" value="{{ data_get($target_comment, 'id') }}">
+                      <div class="c-user-info">
+                        <div class="c-user-name">{{ data_get($target_comment, 'username') }}</div>
+                        <div class="c-user-id">@ {{ data_get($target_comment, 'user_identifier') }}</div>
+                      </div>
                     </div>
-                    <div class="c-form-submit c-button-wrap">
-                      <button type="submit" class="c-button">更新する</button>
-                    </div>
-                  </form>
-                </div>
+                    <!-- <textarea name="comment" id="comment" cols="30" rows="10">{!! nl2br(htmlspecialchars(data_get($target_comment, 'comment'))) !!}</textarea> -->
+                    <textarea name="comment" id="comment" cols="30" rows="10">{{ data_get($target_comment, 'comment') }}</textarea>
+                    @error('comment')
+                    <p class="error-text">※{{ $message }}</p>
+                    @enderror
+                    <input type="hidden" name="topic_id" value="{{ data_get($topic, 'id') }}">
+                    <input type="hidden" name="comment_id" value="{{ data_get($target_comment, 'id') }}">
+                  </div>
+                  <div class="c-form-submit c-button-wrap">
+                    <button type="submit" class="c-button">更新する</button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
     </div>
-    </section>
-    </main>
-    <!-- l-footer START -->
-    @include('components.l-footer')
-    <!-- l-footer END -->
+  </div>
+  </section>
+  </main>
+  <!-- l-footer START -->
+  @include('components.l-footer')
+  <!-- l-footer END -->
   </div>
   </div>
   @include('components.javascript')
